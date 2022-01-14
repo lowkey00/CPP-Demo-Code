b6dbc0729fed82cfe2b5da606be5e7f1412fc95c
lowkey00 <58852975+lowkey00@users.noreply.github.com>
2022-01-14 17:19:46 +0700

    Hackerrank problem

--- aliceandbob'ssillygame.cpp
+++ aliceandbob'ssillygame.cpp
@@ -16,10 +16,8 @@ string sillyGame(int n) {
     vector<bool> p;
     for(int i = 0; i <= n; i++) p.push_back(true);
     p[0] = p[1] = false;
-    for(int i = 2; i * i <= n; i++) {
-        if (p[i]) {
-            for (int j = i * i; j <= n; j += i) p[j] = false;
-        }
+    for (int i = 2; i * i <= n; i++) {
+        if (p[i]) for (int j = i * i; j <= n; j += i) p[j] = false;
     }
     int cnt = 0;
     for (int i = 2; i <= n; i++) if (p[i]) cnt++;
