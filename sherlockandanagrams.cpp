ed375df76a38727963f2a90cc8165719ffdabd75
lowkey00 <58852975+lowkey00@users.noreply.github.com>
2022-01-20 17:08:59 +0700

    Hackerrank problem

---
+++ sherlockandanagrams.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'sherlockAndAnagrams' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+int sherlockAndAnagrams(string s) {
+    int len = s.length();
+    vector<string> v;
+    for (int i = 0; i < len; i++) {
+        string substr = "";
+        for (int j = i; j < len; j++) {
+            substr += s[j];
+            if (substr.length() != len) v.push_back(substr);
+        }
+    }
+    int size = v.size();
+    int ans = 0;
+    map<string, int> mp;
+    for (int i = 0; i < size; i++) {
+        sort(v[i].begin(), v[i].end());
+        mp[v[i]]++;
+    }
+    for (auto i : mp) ans += (((i.second) * (i.second - 1)) / 2);
+    return ans;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string s;
+        getline(cin, s);
+
+        int result = sherlockAndAnagrams(s);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
